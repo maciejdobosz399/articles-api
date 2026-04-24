139deb92d0c9ff3310b70dd5b113356071d6e8fc
Maciej Dobosz <maciej.doobosz@gmail.com>
2026-04-24 09:35:07 +0200

    Add Terraform config and restructure infra folders
    Added article-service.tf for Azure provisioning (resource group, service plan, Linux web app). Refactored solution folders: moved k8s manifests to infra/k8s and added infra/terraform for Terraform files.

---
+++ article-service.tf
@@ -0,0 +1,40 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "articles-api-terraform"
+  location = "polandcentral"
+}
+
+resource "azurerm_service_plan" "plan" {
+  name                = "article-service-terraform-plan"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  os_type  = "Linux"
+  sku_name = "B1"
+}
+
+resource "azurerm_linux_web_app" "app" {
+  name                = "articles-api-webapp"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  service_plan_id     = azurerm_service_plan.plan.id
+
+  site_config {
+    always_on = true
+  }
+}
